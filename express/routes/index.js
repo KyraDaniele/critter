b62cf3574c7f90503f532762b98e1bd6de64d4ba
Kyra Mellema <kemellema@gmail.com>
2019-12-03 08:52:03 -0500

    changed text

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	res.send("What the fuckkkkk").status(200);
+	res.send("Twitter, but for pets!").status(200);
 });
 
 module.exports = router;
