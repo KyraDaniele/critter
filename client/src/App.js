631de738a5107ca8fe7120a3ba14757106e6c979
Kyra Mellema <kemellema@gmail.com>
2019-12-03 10:54:25 -0500

    Revert "added the backend and are making minor changes to the blog"
    This reverts commit 7137c089602771deda03b8795fa4171953e2f8d3.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import ContactForm from "./components/ContactForm";
-import BlogList from "./components/BlogList";
-import BlogPost from "./components/BlogPost";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<ContactForm />
-				<Route path="/" exact component={BlogList} />
-				<Route path="/post/:post_id?" component={BlogPost} />
-			</div>
-		</Router>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
